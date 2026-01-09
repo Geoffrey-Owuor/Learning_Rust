926740f5cf6cd02a775f0dec38dba096dfff217e
Owuor Geoffrey Okinyi <76515367+Geoffrey-Owuor@users.noreply.github.com>
2026-01-09 10:23:21 +0300

    Delete data_types/target directory

--- data_types/target/debug/data_types.d
+++
@@ -1,1 +0,0 @@
-C:\Users\GT009\Downloads\Learning_Rust\data_types\target\debug\data_types.exe: C:\Users\GT009\Downloads\Learning_Rust\data_types\src\main.rs
--- data_types/target/debug/deps/data_types.d
+++
@@ -1,5 +0,0 @@
-C:\Users\GT009\Downloads\Learning_Rust\data_types\target\debug\deps\data_types.d: src\main.rs
-
-C:\Users\GT009\Downloads\Learning_Rust\data_types\target\debug\deps\data_types.exe: src\main.rs
-
-src\main.rs:
