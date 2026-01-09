48cfde078b9619cbbce9c37e8fcae01b7fa06bb3
Geoffrey-Owuor <geoffreyowuor22@gmail.com>
2026-01-09 12:09:57 +0300

    testing mutable references

--- references/src/main.rs
+++ references/src/main.rs
@@ -1,14 +1,28 @@
 // Testing code with references and borrowing
+// References are immutable by default
 fn main() {
-    let s1 = String::from("hello");
+    let mut s1 = String::from("hello");
 
-    // Here we are referencing the value in s1
+    // Here we are referencing the value in s1 -
     let len = calculate_length(&s1);
 
-    println!("The length of '{s1}' is {len}.");
+    // Changing the value of s1
+    change(&mut s1);
+
+    println!("The length of '{s1}' before mutation is {len}.");
+
+    // Check length again
+    let len = calculate_length(&s1);
+
+    println!("The length of {s1} after mutation is {len}.");
 }
 
 //The calculate_length function that has a reference to an object as a parameter instead of transferring ownership of a value
 fn calculate_length(s: &String) -> usize {
     s.len()
+}
+
+// A function for testing mutable references
+fn change(some_string: &mut String) {
+    some_string.push_str(", world");
 }
